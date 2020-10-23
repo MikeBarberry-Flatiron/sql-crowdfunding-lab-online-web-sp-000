8281828ea28b103e2be8c2b429a19244e171f026
Mike Barberry <mikebarberry@protonmail.com>
2020-10-23 07:32:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -52,5 +52,6 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   FROM projects
   JOIN pledges
   ON projects.id = Pledges.project_id
-  WHERE Projects.category = 'books'"
+  WHERE Projects.category = 'books'
+  GROUP BY Projects.category"
 end
